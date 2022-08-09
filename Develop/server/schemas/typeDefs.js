b44f45becb23e07a22af08b634620b13d18e56a4
msnaye <99111936+msnaye@users.noreply.github.com>
2022-08-09 01:35:43 -0500

    working on resolvers

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
-const {gql} = require("apollo-server-express");
+const { gql } = require("apollo-server-express");
 
-const typeDefs =gql`
+const typeDefs = gql`
 type Books{
     bookId: String
     authors: [String]
@@ -9,12 +9,35 @@ type Books{
     link: String
     image: String
 }
+input BookInput{
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    link: String
+    image: String
+
+}
 type User{
-    _id: id
+    _id: ID
     username: String
     email: String
     bookCount: Int
     savedBooks: [Books]
 }
-
+type Auth{
+    token: ID!
+    user: User
+}
+type Query{
+    me: User
+    user(username: String!): User
+    users: [User]
+}
+type Mutation{
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookData): User
+    removeBook(bookId: String): User
+}
 `;
